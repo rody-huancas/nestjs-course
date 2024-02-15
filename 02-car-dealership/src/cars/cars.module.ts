863ace56190a1ae6167af7507a335915c54eb4b8
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-14 20:21:26 -0500

    Add: seeds for cars and brands

--- 02-car-dealership/src/cars/cars.module.ts
+++ 02-car-dealership/src/cars/cars.module.ts
@@ -4,6 +4,7 @@ import { CarsService } from './cars.service';
 
 @Module({
   controllers: [CarsController],
-  providers: [CarsService]
+  providers: [CarsService],
+  exports: [CarsService] // todos tendrán acceso al cars service
 })
 export class CarsModule {}
--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -6,21 +6,11 @@ import { CreateCarDto, UpdateCarDto } from './dto';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
-    {
-      id: uuid(),
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-    {
-      id: uuid(),
-      brand: 'Honda',
-      model: 'Civic',
-    },
-    {
-      id: uuid(),
-      brand: 'Jeep',
-      model: 'Cherokee',
-    },
+    // {
+    //   id: uuid(),
+    //   brand: 'Toyota',
+    //   model: 'Corolla',
+    // }
   ];
 
   findAll() {
@@ -67,5 +57,9 @@ export class CarsService {
   delete(id: string) {
     this.findByOneId(id)
     this.cars = this.cars.filter(car => car.id!== id);
+  }
+
+  fillCarsWithSeedData(cars: Car[]) {
+    this.cars = cars
   }
 }
