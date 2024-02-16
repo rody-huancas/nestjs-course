f10444aad9b169c4f534f4141264e1c98df9be59
Rody Huancas <rodyhuancas.04@gmail.com>
2024-02-16 01:28:14 -0500

    Add: http adapter

---
+++ 03-pokedex/src/seed/seed.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonMoldel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
+  ){}
+
+  async executeSeed() {
+    await this.pokemonMoldel.deleteMany({}); // eliminar todos los registros previos
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?offset=650&limit=650');
+    const pokemonToInsert: {name: string, no: number}[] = [];
+
+    data.results.forEach( async({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[ segments.length - 2 ];
+      // const pokemon = await this.pokemonMoldel.create({ name, no });
+      pokemonToInsert.push({ name, no });
+    })
+
+    await this.pokemonMoldel.insertMany(pokemonToInsert)
+
+    return 'Seed Executed';
+  }
+}
